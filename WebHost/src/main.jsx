e69efd7bf69cc8336af530a38eb84a0bf54ef4e7
River Seeber <riverseeber12@gmail.com>
2026-06-16 10:39:50 -0700

    allow for switching pages

--- WebHost/src/main.jsx
+++ WebHost/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './MDXEditor'
 import './style.css'
 
+let site;
+let page = "index.md";
+let apiStem = "http://localhost:8000";
+
+
+// this snippet injects the MDXEditor into the web app
 const ref = createRef();
 createRoot(document.getElementById('MDXEditorWindow')).render(
     <>
@@ -11,16 +17,57 @@ createRoot(document.getElementById('MDXEditorWindow')).render(
     </>
 );
 
-let site;
+let startingVals;
 
+// set the name for the site. This function allows 
+// you to swap between which site is selected. It does not rename your site.
 function setSite() {
     site = document.getElementById("site").value;
-    console.log(site);
-    loadPage();
+    // get the current list of templates
+    fetch("http://localhost:8000/meta/templates/"+site)
+    .then(function(response){
+        return response.json()
+    }).then(function(data){
+        let templates = data["templates"];
+        for (let i = 0; i < templates.length; i++) {
+            // the filename of the template
+            const template = templates[i];
+            // filename without extension (.md, .html)
+            let template_stripped = template.replace(/\.[^/.]+$/, ""); // https://stackoverflow.com/a/4250408
+            // create an html node for this option
+            let opt = document.createElement("option");
+            opt.value = template;
+            opt.innerHTML = template_stripped;
+            // append this option to the list in the selector
+            document.getElementById("template_selector").appendChild(opt);
+        }
+
+        // fill out the file explorer
+        handleFileIndex();
+
+        //after selecting which site to edit, we load the homepage contents 
+        // for the site into the buffer
+        loadPage();
+    });
 }
 window.setSite = setSite;   //accessible via html
 
+function switchPage(newPage){
+    //save current buffer
+    savePage();
+    page = newPage;
+    //load the new page into the buffer
+    loadPage();
+}
+window.switchPage = switchPage;
+
+// tell the server to run 11ty, updating the deployed site with current 
+// versions of the source files
 function publishSite(){
+    // we should save the site one last time before publishing
+    savePage();
+
+    //okay now actually publish
     fetch("http://localhost:8000/publish/"+site, {
         method: "PUT"
     }).then(function(response) {
@@ -29,10 +76,11 @@ function publishSite(){
 }
 window.publishSite = publishSite;   //accessible via html
 
+//download the page from the server, overwritting the current local buffer
 function loadPage(){
     // decide which page on the site to download
     // (need to make this dynamic later)
-    let page = "index.md";
+    //let page = "index.md";
     document.getElementById("page_title").innerHTML = page;
 
     //fetch the page via the API
@@ -44,8 +92,15 @@ function loadPage(){
     .then(function(data){
         let content = data["content"];
         let frontMatter = data["frontMatter"];
-        console.log(content);
-        
+
+        // we cache this so we can diff against it when we upload, 
+        // saving on network bandwidth
+        startingVals = {
+            "content": content, 
+            "frontMatter": frontMatter, 
+            "template": data["templateName"]
+        };
+
         // save the frontMatter for later when we upload
         document.getElementById("frontMatter").value = frontMatter;
 
@@ -53,6 +108,21 @@ function loadPage(){
     });
 }
 
+function setTemplate(){
+    // get the input element in the html
+    let templateSelector = document.getElementById("template_selector");
+    // get the filename for the selected template
+    let template = templateSelector.options[templateSelector.selectedIndex].value;
+    // get the old frontMatter
+    let frontMatter = document.getElementById("frontMatter").value;
+    // update the frontMatter to use the new template
+    frontMatter = frontMatter.replace(/(?<=layout: ).+/, template);
+    // overwrite our saved value (in the hidden input) for the frontMatter
+    document.getElementById("frontMatter").value = frontMatter;
+}
+window.setTemplate = setTemplate;
+
+//upload the page to the server
 function savePage(){
     // get the hidden frontMatter
     let frontMatter = document.getElementById("frontMatter").value;
@@ -60,12 +130,9 @@ function savePage(){
     let content = ref.current?.getMarkdown();
     let data = {"content": content, "frontMatter": frontMatter};
 
-    console.log("data is: ");
-    console.log(data);
-
     // get the specific page we're editing
     // (this also needs to be dynamic eventually)
-    let page = "index.md";
+    //let page = "index.md";
     //send an `update_page` API to the server
     fetch("http://localhost:8000/update/"+site+"/"+page, {
         method: "PUT",
@@ -79,4 +146,40 @@ function savePage(){
     });
 
 
+}
+
+function handleFileIndex(){
+    getFileIndex_andApply();
+}
+
+function getFileIndex_andApply(){
+    fetch(apiStem+"/meta/index/"+site)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        return applyFileIndex(data);
+    });
+}
+
+function applyFileIndex(index){
+    let fileExplorer = document.getElementById("file_explorer_list");
+    applyFileIndex_recursive(index, fileExplorer);
+}
+
+function applyFileIndex_recursive(index, ul){
+    console.log(index);
+    for (var file in index) {
+        let cleanFile = file.substring(1);
+        let li = document.createElement("li");
+        li.setAttribute("onclick", "switchPage('"+cleanFile+"');");
+        li.innerHTML = file.substring(file.search(/\/([^\/]*)$/) + 1);
+        ul.appendChild(li);
+        // if the file is actually a directory, append its contents
+        if (index[file] != null) {
+            li.setAttribute("onclick", "switchPage('"+cleanFile+"/index.md');");
+            let sub_ul = document.createElement("ul");
+            ul.appendChild(sub_ul);
+            applyFileIndex_recursive(index[file], sub_ul);
+        }
+    }
 }
--- api_handler.py
+++ api_handler.py
@@ -4,14 +4,14 @@ import os, subprocess
 app = FastAPI()
 
 
-@app.get("/site/{site}/{page}")
+@app.get("/site/{site}/{page:path}")
 def get_page(site: str, page: str):
     # read the file
     try:
         with open(f"src/{site}/{page}", "r") as f:
             md = f.read()
     except FileNotFoundError:
-        return -1
+        return {"error": "file not found error"}
 
     # get a version of the md without the frontmatter
     # we will call this version 'content'
@@ -25,19 +25,25 @@ def get_page(site: str, page: str):
     # that isn't a template (_includes/)
     templateName = None
     if not (page.endswith(".md") and page.endswith(".html") and page.startswith("_includes/")):
-        # I'm sure there's a better way to do this, but it works
-        templateStart = frontMatter.find("layout: ") + len("layout: ")
-        templateEnd = frontMatter.find("\n", templateStart)
+        templateStart, templateEnd = getTemplateIndices(frontMatter)
         templateName = frontMatter[templateStart:templateEnd]
         # we need to add error checking to this
 
     return {"md": md, "content": content, "frontMatter":frontMatter, "templateName": templateName}
 
+# utility function that gets the indices of the layout file (following the "layout: ") 
+# in a frontMatter string
+def getTemplateIndices(frontMatter: str):
+    # I'm sure there's a better way to do this, but it works
+    start = frontMatter.find("layout: ") + len("layout: ")
+    end = frontMatter.find("\n", start)
+    return start, end
+
 # This API either creates a new page, or updates an existing page
 # you only need to pass in a dictionary containing
 # "frontMatter" and "content". All other fields are unused.
 # this API will concatenate them in order to save back to the md file
-@app.put("/update/{site}/{page}")
+@app.put("/update/{site}/{page:path}")
 def update_page(site: str, page: str, update: dict):
     if site not in os.listdir("src"):
         raise HTTPException(status_code=404, detail="Site not found")
@@ -71,3 +77,44 @@ def publish_site(site: str):
     subprocess.run(["./publishSite.sh", site])
     # we need to add error checking at some point
     return
+
+# return a list of templates currently "installed" on the site
+@app.get("/meta/templates/{site}")
+def get_templates(site: str):
+    ls = os.listdir(f"src/{site}/_includes/")
+    # get a list of all files that end with .html or .md
+    templates = [t for t in ls if (t.endswith(".html") or t.endswith(".md"))]
+    return {"templates": templates}
+
+@app.put("/meta/templates/{site}/{page:path}")
+def set_template(site: str, page: str, newTemplate: str):
+    myPage = get_page(site, page)
+    # if it's already done, do nothing
+    if newTemplate == myPage["templateName"]:
+        return
+    # get the content and frontMatter
+    content = myPage["content"]
+    frontMatter = myPage["frontMatter"]
+    # get the indices of the existing template
+    start, end = getTemplateIndices(frontMatter)
+    # update it with our new template we're using
+    frontMatter = f"{frontMatter[:start]}{newTemplate}{frontMatter[end:]}"
+    # update the source file with our new frontMatter
+    update_page(site, page, {"content": content, "frontMatter": frontMatter})
+
+
+@app.get("/meta/index/{site}")
+def get_index(site: str):
+    # recursively go through the subdirs of the site folder, listing everything
+    return get_index_recursive(site, "")
+
+def get_index_recursive(site, subPath):
+    fullPath = f"src/{site}/{subPath}"
+    ls = os.listdir(fullPath)
+    index = {}
+    for item in ls:
+        if os.path.isdir(f"{fullPath}/{item}"):
+            index.update({f"{subPath}/{item}": get_index_recursive(site, f"{subPath}/{item}")})
+        else:
+            index.update({f"{subPath}/{item}": None})
+    return index
